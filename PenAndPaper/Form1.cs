f43bd500a047859a21ba307c619db78524db2c77
KIMBSQT <85073880+KIMBSQT@users.noreply.github.com>
2021-05-31 15:29:23 +0200

    Program

---
+++ PenAndPaper/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_r_layeh
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
