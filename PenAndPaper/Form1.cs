25d3426c62a7f849e81daa3839fd51d06eb5e31b
Michał Zawadzki <zm46704@zut.edu.pl>
2021-06-07 10:18:12 +0200

    cos2

--- PenAndPaper/Form1.cs
+++ PenAndPaper/Form1.cs
@@ -16,5 +16,10 @@ namespace Project_r_layeh
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
