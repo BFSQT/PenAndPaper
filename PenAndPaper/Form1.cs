3eb93826e40afe10e8de55758ffcf5cd18c8c41d
KIMBSQT <85073880+KIMBSQT@users.noreply.github.com>
2021-06-07 10:15:22 +0200

    s

--- PenAndPaper/Form1.cs
+++ PenAndPaper/Form1.cs
@@ -16,5 +16,10 @@ namespace Project_r_layeh
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
