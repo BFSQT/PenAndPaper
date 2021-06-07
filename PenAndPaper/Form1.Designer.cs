8d7f024787c287fd948e77c3f730e35dde0e20b6
Michał Zawadzki <zm46704@zut.edu.pl>
2021-06-07 10:24:54 +0200

    naprawa

--- PenAndPaper/Form1.Designer.cs
+++ PenAndPaper/Form1.Designer.cs
@@ -53,7 +53,7 @@ namespace Project_r_layeh
             this.button2.TabIndex = 2;
             this.button2.Text = "Wybór kości";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            
             // 
             // button1
             // 
